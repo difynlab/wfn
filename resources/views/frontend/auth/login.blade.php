7eb8a0799972416dcc791275f3e514b14efed0ee
M.B. Sharqi Sajjath mohammath <90747178+MBSharqi@users.noreply.github.com>
2025-06-17 17:39:25 +0530

    Responsive update

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -10,11 +10,11 @@
 
     <div class="container page-global login">
         <div class="row">
-            <div class="col-4 left">
+            <div class="col-8 col-md-6 left">
                 <img src="{{ asset('storage/frontend/login.jpg') }}" alt="Log In" class="image">
             </div>
 
-            <div class="col-8 right">
+            <div class="col-8 col-md-6 right">
                 <p class="title">Get Started Now</p>
                 <p class="description">Log in to access portal, track progress, and manage requests</p>
 
