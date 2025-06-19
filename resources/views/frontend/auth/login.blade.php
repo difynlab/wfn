063b08746a2cf03387b3e8685d21bfdbd65d3fe6
imzajjithvedha <zajjith@gmail.com>
2025-06-19 10:49:01 +0530

    reviewed

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -10,11 +10,11 @@
 
     <div class="container page-global login">
         <div class="row">
-            <div class="col-lg-4 col-md-6 col-sm-12 left">
+            <div class="col-12 col-md-6 col-lg-4 left">
                 <img src="{{ asset('storage/frontend/login.jpg') }}" alt="Log In" class="image">
             </div>
 
-            <div class="col-lg-8 col-md-6 col-sm-12 right">
+            <div class="col-12 col-md-6 col-lg-8 right">
                 <p class="title">Get Started Now</p>
                 <p class="description">Log in to access portal, track progress, and manage requests</p>
 
