f2dbfd58f754b88104d9cd34715c39984bcbf65f
M.B. Sharqi Sajjath mohammath <90747178+MBSharqi@users.noreply.github.com>
2025-07-04 18:16:55 +0530

    home page dynamic

--- app/Http/Controllers/Frontend/Website/HomepageController.php
+++ app/Http/Controllers/Frontend/Website/HomepageController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Frontend\Website;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\HomepageContent;
 
 class HomepageController extends Controller
 {
     public function index(Request $request)
     {
-        return view('frontend.website.homepage');
+        $contents = HomepageContent::find(1);
+
+        return view('frontend.website.homepage', [
+            'contents' => $contents
+        ]);
     }
 }
