8b2a45bd760609126762cc156419e954033ec223
Zajjith Vedha <zajjith@gmail.com>
2025-08-28 11:34:45 +0530

    test github desktop

--- public/frontend/js/recaptcha.js
+++ public/frontend/js/recaptcha.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
         grecaptcha.execute(window.siteKey, { action: window.recaptchaAction })
         .then(function (token) {
             document.getElementById('recaptcha_token').value = token;
+            console.log('Test GitHub Desktop');
         })
         .catch(() => {
             alert('reCAPTCHA failed to load. Please disable blockers or try again.');
