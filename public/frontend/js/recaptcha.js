0ba6fa14786f66aca95319e7f2ee4007e5489011
imzajjithvedha <zajjith@gmail.com>
2025-08-28 11:11:01 +0530

    recpatcha

--- public/frontend/js/recaptcha.js
+++ public/frontend/js/recaptcha.js
@@ -1,11 +1,8 @@
-document.getElementById('form').addEventListener('submit', function (e) {
-    e.preventDefault();
-
+$(document).ready(function() {
     grecaptcha.ready(function () {
         grecaptcha.execute(window.siteKey, { action: window.recaptchaAction })
         .then(function (token) {
             document.getElementById('recaptcha_token').value = token;
-            e.target.submit();
         })
         .catch(() => {
             alert('reCAPTCHA failed to load. Please disable blockers or try again.');
