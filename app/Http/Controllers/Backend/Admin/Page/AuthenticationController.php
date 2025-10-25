f4d8d206219e2d0db9900215f217640b5297dabe
Zajjith Vedha <zajjith@gmail.com>
2025-10-25 13:37:44 +0530

    register image function

--- app/Http/Controllers/Backend/Admin/Page/AuthenticationController.php
+++ app/Http/Controllers/Backend/Admin/Page/AuthenticationController.php
@@ -44,8 +44,10 @@ class AuthenticationController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'new_login_image' => 'nullable|max:30720',
+            'new_register_image' => 'nullable|max:30720',
         ], [
-            'new_login_image.max' => 'Image must not be greater than 30 MB.'
+            'new_login_image.max' => 'Image must not be greater than 30 MB.',
+            'new_register_image.max' => 'Image must not be greater than 30 MB.'
         ]);
 
         if($validator->fails()) {
@@ -80,12 +82,37 @@ class AuthenticationController extends Controller
             }
         // Login image
 
+        // Register image
+            if($request->file('new_register_image')) {
+                if($request->old_register_image) {
+                    Storage::delete('backend/pages/' . $request->old_register_image);
+                }
+
+                $new_register_image = $request->file('new_register_image');
+                $register_image_name = Str::random(40) . '.' . $new_register_image->getClientOriginalExtension();
+                $new_register_image->storeAs('backend/pages', $register_image_name);
+            }
+            else if($request->old_register_image == null) {
+                if($contents->{'register_image_' . $short_code}) {
+                    Storage::delete('backend/pages/' . $contents->{'register_image_' . $short_code});
+                }
+
+                $register_image_name = null;
+            }
+            else {
+                $register_image_name = $request->old_register_image;
+            }
+        // Register image
+
         $data = $request->except(
             'old_login_image',
             'new_login_image',
+            'old_register_image',
+            'new_register_image',
         );
 
         $data['login_image_' . '' . $short_code] = $login_image_name;
+        $data['register_image_' . '' . $short_code] = $register_image_name;
         $contents->fill($data)->save();
 
         return redirect()->back()->with('success', "Successfully updated!");
