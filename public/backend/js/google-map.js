898f75f6b303f7d391ae14e6fdb3a9becc7864de
Zajjith Vedha <zajjith@gmail.com>
2025-12-11 09:35:23 +0530

    locality

--- public/backend/js/google-map.js
+++ public/backend/js/google-map.js
@@ -1,11 +1,11 @@
 const apiKey = document.currentScript.dataset.mapsKey;
 const nativeAttachShadow = Element.prototype.attachShadow;
 
-Element.prototype.attachShadow = function(init) {
-    if(this.localName === 'gmp-place-autocomplete') {
-        const root = nativeAttachShadow.call(this, { ...init, mode: 'open' });
+Element.prototype.attachShadow = function (init) {
+    if (this.localName === "gmp-place-autocomplete") {
+        const root = nativeAttachShadow.call(this, { ...init, mode: "open" });
 
-        const style = document.createElement('style');
+        const style = document.createElement("style");
         style.textContent = `
             .widget-container {
                 border: 0 !important;
@@ -27,9 +27,43 @@ Element.prototype.attachShadow = function(init) {
     return nativeAttachShadow.call(this, init);
 };
 
-(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+((g) => {
+    var h,
+        a,
+        k,
+        p = "The Google Maps JavaScript API",
+        c = "google",
+        l = "importLibrary",
+        q = "__ib__",
+        m = document,
+        b = window;
+    b = b[c] || (b[c] = {});
+    var d = b.maps || (b.maps = {}),
+        r = new Set(),
+        e = new URLSearchParams(),
+        u = () =>
+            h ||
+            (h = new Promise(async (f, n) => {
+                await (a = m.createElement("script"));
+                e.set("libraries", [...r] + "");
+                for (k in g)
+                    e.set(
+                        k.replace(/[A-Z]/g, (t) => "_" + t[0].toLowerCase()),
+                        g[k]
+                    );
+                e.set("callback", c + ".maps." + q);
+                a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+                d[q] = f;
+                a.onerror = () => (h = n(Error(p + " could not load.")));
+                a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+                m.head.append(a);
+            }));
+    d[l]
+        ? console.warn(p + " only loads once. Ignoring:", g)
+        : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)));
+})({
     key: apiKey,
-    v: "weekly"
+    v: "weekly",
 });
 
 async function initMap() {
@@ -37,65 +71,98 @@ async function initMap() {
     const { Geocoder } = await google.maps.importLibrary("geocoding");
 
     const placeAutocomplete = new google.maps.places.PlaceAutocompleteElement();
-    document.getElementById('place-autocomplete-card').appendChild(placeAutocomplete);
+    document
+        .getElementById("place-autocomplete-card")
+        .appendChild(placeAutocomplete);
 
     const pickComponent = (components, types) =>
-        (components || []).find(c => c.types.some(t => types.includes(t))) || null;
+        (components || []).find((c) =>
+            c.types.some((t) => types.includes(t))
+        ) || null;
 
-    placeAutocomplete.addEventListener('gmp-select', async ({ placePrediction }) => {
-        const place = placePrediction.toPlace();
-        await place.fetchFields({
-            fields: ['displayName', 'formattedAddress', 'addressComponents', 'location']
-    });
+    placeAutocomplete.addEventListener(
+        "gmp-select",
+        async ({ placePrediction }) => {
+            const place = placePrediction.toPlace();
 
-    document.getElementById('address_name').value = place.displayName || '';
-    document.getElementById('address_en').value = place.formattedAddress || '';
+            await place.fetchFields({
+                fields: [
+                    "displayName",
+                    "formattedAddress",
+                    "addressComponents",
+                    "location",
+                ],
+            });
 
-    const cityEN = pickComponent(
-        place.addressComponents,
-        [
-            'locality',
-            'postal_town',
-            'administrative_area_level_2', 'administrative_area_level_1',
-            'sublocality_level_1'
-        ]
-    );
+            document.getElementById("address_name").value =
+                place.displayName || "";
+            document.getElementById("address_en").value =
+                place.formattedAddress || "";
 
-    document.getElementById('city_en').value = cityEN?.longText || cityEN?.long_name || '';
+            const locality = pickComponent(place.addressComponents, [
+                "locality",
+            ]);
 
-    document.getElementById('latitude' ).value = place.location.lat().toFixed(7);
-    document.getElementById('longitude').value = place.location.lng().toFixed(7);
+            let cityEN = null;
 
-
-    const geocoder = new google.maps.Geocoder();
-
-    geocoder.geocode(
-        {
-            location: place.location,
-            language: 'ar'
-        },
-        (results, status) => {
-            if(status === 'OK' && results?.length) {
-                const first = results[0];
-
-                document.getElementById('address_ar').value = first.formatted_address || '';
-
-                const cityAR = (first.address_components || []).find(comp =>
-                    comp.types.some(type =>
-                        [
-                            'locality', 'postal_town', 'administrative_area_level_2', 'administrative_area_level_1', 'sublocality_level_1'
-                        ].includes(type)
-                    )
-                );
-
-                document.getElementById('city_ar').value = cityAR?.long_name || '';
+            if (locality) {
+                cityEN = locality;
+            } else {
+                cityEN = pickComponent(place.addressComponents, [
+                    "sublocality_level_1",
+                    "administrative_area_level_1",
+                    "administrative_area_level_2",
+                    "postal_town",
+                    "locality",
+                ]);
             }
-            else {
-                console.error('Geocoder failed:', status);
-            }
+
+            document.getElementById("city_en").value =
+                cityEN?.longText || cityEN?.long_name || "";
+
+            document.getElementById("latitude").value = place.location
+                .lat()
+                .toFixed(7);
+            document.getElementById("longitude").value = place.location
+                .lng()
+                .toFixed(7);
+
+            const geocoder = new google.maps.Geocoder();
+
+            geocoder.geocode(
+                {
+                    location: place.location,
+                    language: "ar",
+                },
+                (results, status) => {
+                    if (status === "OK" && results?.length) {
+                        const first = results[0];
+
+                        document.getElementById("address_ar").value =
+                            first.formatted_address || "";
+
+                        const cityAR = (first.address_components || []).find(
+                            (comp) =>
+                                comp.types.some((type) =>
+                                    [
+                                        "locality",
+                                        "postal_town",
+                                        "administrative_area_level_2",
+                                        "administrative_area_level_1",
+                                        "sublocality_level_1",
+                                    ].includes(type)
+                                )
+                        );
+
+                        document.getElementById("city_ar").value =
+                            cityAR?.long_name || "";
+                    } else {
+                        console.error("Geocoder failed:", status);
+                    }
+                }
+            );
         }
-        );
-    });
+    );
 }
 
 initMap();
