17815a022ede60dd132bacbf34e1ae3d135df5a8
Zajjith Vedha <zajjith@gmail.com>
2026-02-20 10:06:02 +0530

    warehouse seeder removed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             StorageTypeSeeder::class,
-            WarehouseSeeder::class,
+            // WarehouseSeeder::class,
             HomepageContentSeeder::class,
             SettingsSeeder::class,
             AuthenticationContentSeeder::class,
